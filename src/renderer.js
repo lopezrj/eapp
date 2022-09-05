76f73f44d15104f94272f763e5aa9fba706eadc1
lopezrj <lopezrj@hotmail.com>
2022-09-05 13:52:00 -0600

    Fist commit

---
+++ src/renderer.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+const ipcRenderer = electron.ipcRenderer;
+
+ipcRenderer.on('cpu', (event,data) => {
+  document.getElementById('cpu').innerHTML = data.toFixed(2);
+})
+ipcRenderer.on('mem', (event,data) => {
+  document.getElementById('mem').innerHTML = data.toFixed(2);
+})
+ipcRenderer.on('total-mem', (event,data) => {
+  document.getElementById('total-mem').innerHTML = data.toFixed(2);
+})
